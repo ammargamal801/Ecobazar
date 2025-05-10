d47b022e1a88e64edd923b10eb74bf8eb5f555be
Maryam Eid <160788995+marec1044@users.noreply.github.com>
2025-05-11 01:16:53 +0300

    .

--- FrontEnd/pages/Wishlist Page/wishlist.php
+++ FrontEnd/pages/Wishlist Page/wishlist.php
@@ -23,6 +23,7 @@ if ($is_logged_in) {
     <!-- Font Awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../../Style/wishlist.css">
+    <link rel="stylesheet" href="../../Style/main.css">
     
 </head>
 <body>
@@ -96,7 +97,9 @@ if ($is_logged_in) {
             </div>
         </div>
     </div>
-
+<?php 
+include '../../pages/footer.html';
+?>
     <!-- Bootstrap JS Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
